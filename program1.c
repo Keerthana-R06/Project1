9b90a1716b76b049fc83ed73afb5d15f26ed245e
Keerthana-R06 <pushparaju242@gmail.com>
2025-09-29 11:22:07 +0530

    all files added

--- program1.c
+++ program1.c
@@ -3,10 +3,7 @@
 int main()
 {
 printf("Helloo....I am Keerthana");
+printf("I am staying in Tumkur this week!!");
 return 0;
 }
-nwq
 
-
-nwq
-
---
+++ program2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+printf("Where are you???Lets meet on this Wednesday at Tumkur Dasara festival.");
+return 0;
+}
